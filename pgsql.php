7ac608e8940ae7da288300ec541caa5d9fb27a5b
fumiaki shimazu <blood.opposer@gmail.com>
2015-12-03 18:08:03 +0900

    Update pgsql.php

--- pgsql.php
+++ pgsql.php
@@ -3,41 +3,48 @@ namespace util;
 
 class PgSQL {
     /**
-     * PHP の配列を PostgreSQL の VALUES リストに変換
+     * HashMap を PostgreSQL の VALUES リストに変換
      * 
-     * dirty data はこのメソッドによる変換を行わないこと
+     *  * dirty data はこのメソッドによる変換を行わないこと
+     *  * 一要素以上必ず存在する前提
      * @param array $array 配列
      * @param string $tblName 仮テーブル名
      * @return string VALUES リスト文字列
      */
-    private function arrayToPgValuesList(array $array, $tblName)
+    public static function mapToPgValuesList(array $array, $tblName)
     {
-        $result = "";
-        if (!empty($array[0])) {
-            $result = "(VALUES ";
-            // 一番最初の要素から key 名を取得
-            $keyList = array_keys($array[0]);
-
-            foreach ($array as $value) {
-                // すべての要素をクォートする
-                $quotedData = array_map(function ($v) {
-                    return "'${v}'";
-                }, $value);
-
-                $result .= "(" . implode(",", $quotedData) . "),";
-            }
-
-            // 最後の文字を削る
-            $result = substr($result, 0, -1);
-            $result .= ") as ${tblName} (". implode(",", $keyList) .")";
+        if (empty($array[0])) {
+            return false;
         }
+
+        $result = "(VALUES ";
+
+        // 一番最初の要素から key 名を取得
+        $keyList = array_keys($array[0]);
+
+        foreach ($array as $value) {
+            // すべての要素をクォートする
+            $quotedData = array_map(function ($v) {
+                return "'${v}'";
+            }, $value);
+            $result .= "(" . implode(",", $quotedData) . "),";
+        }
+        // 最後の文字を削る
+        $result = substr($result, 0, -1);
+        $result .= ") as ${tblName}(". implode(",", $keyList) .")";
         return $result;
     }
-    /*
-    test:
-    echo arrayToPgValuesList([[
+}
+
+/*
+echo PgSQL::mapToPgValuesList([
+    [
         "a"=>1,    
         "n"=>2    
-    ]], "hoge");
-    */
-}
+    ],
+    [
+        "a"=>4,    
+        "n"=>222    
+    ]
+], "hoge");
+*/
